99a605990282a50e787e928396fe0031f54e9436
dhia-djobbi <dhia.djobbi@avaxia-group.com>
2021-11-14 16:59:44 +0100

    secured the routes using IsGranted and security.yaml file

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,13 +5,15 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/article")
+ * @IsGranted("ROLE_ADMIN")
  */
 class ArticleController extends AbstractController
 {
